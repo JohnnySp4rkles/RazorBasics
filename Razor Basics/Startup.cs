6bbf07302ef8f6339be3c1dd813580c21f4ba656
JDiamond <jdiamond@keyedin.com>
2018-09-26 16:31:03 +0100

    changed from a service to a ContainerBuilder!

--- Razor Basics/Startup.cs
+++ Razor Basics/Startup.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorBasics.Infrastructure;
 using RazorBasics.Services;
 
 namespace Razor_Basics {
@@ -20,16 +24,28 @@ namespace Razor_Basics {
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public IServiceProvider ConfigureServices(IServiceCollection services) {
             services.Configure<CookiePolicyOptions>(options => {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddScoped<IInventoryService, InventoryService>();
-            services.AddSingleton<ISurveyService, SurveyService>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // MVC setup
+            //services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddMvc();
+            services.Configure<RazorViewEngineOptions>(opts => {
+                opts.ViewLocationExpanders.Clear();
+                opts.ViewLocationExpanders.Add(new ThemeExpander());
+            });
+
+            // Autofac setup
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            builder.RegisterType<InventoryService>().As<IInventoryService>();
+            builder.RegisterType<SurveyService>().As<ISurveyService>();
+            builder.RegisterType<FormDataService>().As<IFormDataService>();
+            return new AutofacServiceProvider(builder.Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
