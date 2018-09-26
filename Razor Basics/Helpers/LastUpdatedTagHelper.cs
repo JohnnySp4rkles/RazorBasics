f5bbfb8d0f4d0aa9b878d998dbcedd376aef6b85
JDiamond <jdiamond@keyedin.com>
2018-09-26 16:26:08 +0100

    Created html helper classes

---
+++ Razor Basics/Helpers/LastUpdatedTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace RazorBasics.Helpers {
+
+    [HtmlTargetElement("span", Attributes ="last-updated")]
+    public class LastUpdatedTagHelper : TagHelper {
+        public DateTime LastUpdated { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            if(LastUpdated < DateTime.Now.AddDays(-3)){
+                output.Attributes.SetAttribute("class", "text-danger pull-right");
+            }
+        }
+    }
+}
